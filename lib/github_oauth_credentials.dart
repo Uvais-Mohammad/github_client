f9df51d0fa969c9baf66041a4ee415e68b8fb69d
Uvais-Mohammad <uvaiskc55@gmail.com>
2022-11-16 09:43:43 +0530

    Authentication completed

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = '1efaff4961658b42e2c6';
+const githubClientSecret = 'b88154c41fe57890ef5be194edf29adb50090df1';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
